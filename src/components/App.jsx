301da5e05b9d69be4e5f6139fa04899e9d54674e
Haidai Kostiantyn <thekos0596@gmail.com>
2023-04-09 00:15:20 +0300

    create Statistics component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,7 @@
-import { Profile } from "./Profile/Profile";
+import { Profile } from './Profile/Profile';
 import user from '../json/user.json';
+import { Statistics } from './Statistics/Statistics';
+import data from '../json/data.json';
 
 export const App = () => {
   return (
@@ -11,6 +13,7 @@ export const App = () => {
         avatar={user.avatar}
         stats={user.stats}
       />
+      <Statistics title='Upload stats' stats={data} />
     </div>
   );
 };
---
+++ src/components/Statistics/Statistics.jsx
@@ -0,0 +1,29 @@
+import PropTypes from 'prop-types';
+
+export const Statistics = ({ title, stats }) => {
+  return (
+    <section className='statistics'>
+      <h2 className='title'>{title}</h2>
+      <ul className='stat-list'>
+        {stats.map(({ id, label, percentage }) => (
+          <li key={id} className='item'>
+            <span className='label'>{label}</span>
+            <span className='percentage'>{percentage}</span>
+          </li>
+        ))}
+      </ul>
+    </section>
+  );
+};
+
+Statistics.propTypes = {
+  stats: PropTypes.arrayOf(
+    PropTypes.exact([
+      {
+        id: PropTypes.string.isRequired,
+        label: PropTypes.string.isRequired,
+        percentag: PropTypes.number.isRequired,
+      },
+    ])
+  ),
+};
